cbdf555b40c1b692161441e459f5ba319f04a0ce
Fraham <frahamp@gmail.com>
2017-02-19 19:25:15 +0000

    Added base html classes

---
+++ SudokuSolver/SudokuSolver.Repositories/Input/HTML/Reader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SudokuSolver.Repositories.Input.HTML
+{
+    public class Reader : IInput
+    {
+        private IEnumerable<string> avaiableWebsites = new List<string> { "" };
+
+        public int[] GetGrid(string stringURL)
+        {
+            if (string.IsNullOrWhiteSpace(stringURL))
+            {
+                throw new ArgumentException("URL is not valid");
+            }
+
+            Uri url = new Uri(stringURL);
+
+            if (!avaiableWebsites.Contains(url.Host.ToLower()))
+            {
+                throw new ArgumentException($"{stringURL} is not supported");
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SudokuSolver/SudokuSolverTests/Repositories/Input/HTML/ReaderTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using html = SudokuSolver.Repositories.Input.HTML;
+
+namespace SudokuSolverTests.Repositories.Input.HTML
+{
+    [TestFixture]
+    public class ReaderTests
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void ReaderHTML_GetGrid_InvalidFileName_Failure(string fileName)
+        {
+            var reader = new html.Reader();
+            var ex = Assert.Throws<ArgumentException>(delegate { reader.GetGrid(fileName); });
+
+            Assert.AreEqual("URL is not valid", ex.Message);
+        }
+
+        [TestCase("cheese")]
+        [TestCase("..")]
+        [TestCase(".hello.")]
+        public void ReaderHTML_GetGrid_InvalidURL_Failure(string fileName)
+        {
+            var reader = new html.Reader();
+            var ex = Assert.Throws<UriFormatException>(delegate { reader.GetGrid(fileName); });
+
+            Assert.AreEqual("Invalid URI: The format of the URI could not be determined.", ex.Message);
+        }
+
+        [TestCase("http://google.com")]
+        [TestCase("http://facebook.com")]
+        [TestCase("http://bbc.co.uk")]
+        public void ReaderHTML_GetGrid_URLNotFound_Failure(string fileName)
+        {
+            var reader = new html.Reader();
+            var ex = Assert.Throws<ArgumentException>(delegate { reader.GetGrid(fileName); });
+
+            Assert.AreEqual($"{fileName} is not supported", ex.Message);
+        }
+    }
+}
